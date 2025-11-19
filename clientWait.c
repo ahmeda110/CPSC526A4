d30092937ba30e00fe5ca37da01c97b7d0861aa8
ahmeda110 <65793945+ahmeda110@users.noreply.github.com>
2025-11-18 18:41:09 -0700

    Update clientWait.c

--- clientWait.c
+++ clientWait.c
@@ -1,10 +1,10 @@
+// client_wait.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/tcp.h>
-#include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 4) {
@@ -17,34 +17,40 @@ int main(int argc, char *argv[]) {
     char *msg = argv[3];
 
     int s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        perror("socket");
+        return 1;
+    }
 
     struct sockaddr_in srv;
+    memset(&srv, 0, sizeof(srv));
     srv.sin_family = AF_INET;
     srv.sin_port = htons(port);
     inet_pton(AF_INET, server_ip, &srv.sin_addr);
 
-    connect(s, (struct sockaddr*)&srv, sizeof(srv));
+    if (connect(s, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
+        perror("connect");
+        close(s);
+        return 1;
+    }
     printf("Client connected to %s:%d\n", server_ip, port);
 
     struct sockaddr_in local;
     socklen_t len = sizeof(local);
-    getsockname(s, (struct sockaddr*)&local, &len);
+    if (getsockname(s, (struct sockaddr*)&local, &len) == 0) {
+        printf("Client source port: %d\n", ntohs(local.sin_port));
+    }
 
-    printf("Client source port: %d\n", ntohs(local.sin_port));
+    fflush(stdout);
 
-    // -----------------------------
-    // Print TCP seq + ack directly
-    // -----------------------------
-    struct tcp_info info;
-    socklen_t info_len = sizeof(info);
-    getsockopt(s, IPPROTO_TCP, TCP_INFO, &info, &info_len);
-
-    printf("USE THESE VALUES FOR ATTACK:\n");
-    printf("  client seq = %u\n", info.tcpi_unacked);
-    printf("  client ack = %u\n", info.tcpi_snd_mss);
-
-    printf("\nSleeping 12 seconds... Perform your attack now.\n");
+    // Optional small delay so handshake shows up clearly in pcap
     sleep(210);
+
+    printf("Now perform your attack using this source port, then press ENTER to send data...\n");
+    fflush(stdout);
+
+    // Wait for ENTER
+    getchar();
 
     send(s, msg, strlen(msg), 0);
     close(s);
